10078e7a3c824c7449c0c1623b7d40373c8bd05f
David Brown <gatech55@gmail.com>
2020-10-13 03:10:33 -0400

    added http to the api call.  did not have it there and got errors

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,26 @@
 $(document).ready(function () {
+  //console log
   console.log("Hello World");
-
+  // function
   function searchCity(city) {
     var queryURL =
-      "api.openweathermap.org/data/2.5/weather?q=" +
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
       city +
-      "&appid=a0d7c3074ce18b5e27c1432a4af93068";
-    $.ajax;
+      "&units=imperial&appid=a0d7c3074ce18b5e27c1432a4af93068";
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+    });
   }
+
+  // Event handler for user clicking the search button
+  $("#searchBtn").on("click", function (event) {
+    event.preventDefault();
+    var inputCity = $("#searchInput").val().trim();
+    searchCity(inputCity);
+  });
 });
+
+// api.openweathermap.org/data/2.5/weather?q=Atlanta&units=imperial&appid=a0d7c3074ce18b5e27c1432a4af93068
