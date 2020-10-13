0b6d5898a2538561daace95a861c81a674163d2d
David Brown <gatech55@gmail.com>
2020-10-13 02:46:13 -0400

    added supporting files for the website

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  console.log("Hello World");
+
+  function searchCity(city) {
+    var queryURL =
+      "api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=a0d7c3074ce18b5e27c1432a4af93068";
+    $.ajax;
+  }
+});
