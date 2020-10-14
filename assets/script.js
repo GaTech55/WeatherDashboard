93b20d033e24ad012ce2402e060f02ff4f1837e3
David Brown <gatech55@gmail.com>
2020-10-14 09:20:00 -0400

    added new api calls for the uv index and 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   //console log
   console.log("What's the good word?");
-  var currentDay = moment().format("(MM/d/YYYY)");
+  var currentDay = moment().format("(MM/D/YYYY)");
   // function
   function searchCity(city) {
     var queryURL =
@@ -14,9 +14,11 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response);
       var cityName = $("<h3>").text(response.name);
+      cityName.addClass("d-inline");
       var dateVar = $("<h3>").text(currentDay);
+      dateVar.addClass("d-inline");
       var tempVar = $("<p>").text(
-        "Temperature: " + response.main.temp + "&deg; F"
+        "Temperature: " + response.main.temp + " deg F"
       );
       var humidityVar = $("<p>").text(
         "Humidity: " + response.main.humidity + "%"
@@ -25,16 +27,17 @@ $(document).ready(function () {
         "Wind Speed: " + response.wind.speed + " MPH"
       );
       var uvVar = $("<p>").text(response.main.temp);
-      var latVar = $("<p>").text(response.coord.lat);
-      var lonVar = $("<p>").text(response.coord.lat);
+      var latVar = response.coord.lat;
+      var lonVar = response.coord.lon;
+      console.log(response.coord.lat);
+      console.log(response.coord.lon);
       var weatherIcon = $("<img>").attr(
         "src",
         "http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png"
       );
-      console.log(parseInt(latVar));
-      console.log(parseInt(lonVar));
+
       $("#cardText").empty();
-      $("#cardText").attr("class", "d-inline");
+      //   $("#cardText").attr("class", "d-inline");
       $("#cardText").append(
         cityName,
         dateVar,
@@ -43,16 +46,42 @@ $(document).ready(function () {
         humidityVar,
         windVar
       );
-      //   $("#cardText").append(weatherIcon);
+
+      var queryUVindex =
+        "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+        latVar +
+        "&lon=" +
+        lonVar +
+        "&appid=a0d7c3074ce18b5e27c1432a4af93068";
+      console.log(queryUVindex);
+      $.ajax({
+        url: queryUVindex,
+        method: "GET",
+      }).then(function (responseUV) {
+        console.log(responseUV);
+        var uvValue = $("<div>").text("UV Index: " + responseUV.value);
+        console.log(responseUV.value);
+        $("#cardText").append(uvValue);
+      });
+
+      //   https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+      var queryUrlForecast =
+        "http://api.openweathermap.org/data/2.5/onecall?lat=" +
+        latVar +
+        "&lon=" +
+        lonVar +
+        "&appid=a0d7c3074ce18b5e27c1432a4af93068";
+      console.log(queryUrlForecast);
     });
-    // function uvIndex()
   }
 
   // Event handler for user clicking the search button
   $("#searchBtn").on("click", function (event) {
     event.preventDefault();
+    var timeValue = inputCity;
     var inputCity = $("#searchInput").val().trim();
     searchCity(inputCity);
+    localStorage.setItem(timeValue, inputCity);
   });
 });
 
