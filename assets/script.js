727f508230d287dc31a998da0aad577474ad3606
David Brown <gatech55@gmail.com>
2020-10-14 23:24:08 -0400

    Made updates to the card within JS

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
   //console log
   console.log("What's the good word?");
   var currentDay = moment().format("(MM/D/YYYY)");
+  var apiKey = "a0d7c3074ce18b5e27c1432a4af93068";
   // function
   function searchCity(city) {
     var queryURL =
       "http://api.openweathermap.org/data/2.5/weather?q=" +
       city +
-      "&units=imperial&appid=a0d7c3074ce18b5e27c1432a4af93068";
+      "&units=imperial&appid=" +
+      apiKey;
     $.ajax({
       url: queryURL,
       method: "GET",
@@ -18,7 +20,7 @@ $(document).ready(function () {
       var dateVar = $("<h3>").text(currentDay);
       dateVar.addClass("d-inline");
       var tempVar = $("<p>").text(
-        "Temperature: " + response.main.temp + " deg F"
+        "Temperature: " + response.main.temp + " \xB0 "
       );
       var humidityVar = $("<p>").text(
         "Humidity: " + response.main.humidity + "%"
@@ -64,14 +66,24 @@ $(document).ready(function () {
         $("#cardText").append(uvValue);
       });
 
-      //   https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+      //   api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
       var queryUrlForecast =
-        "http://api.openweathermap.org/data/2.5/onecall?lat=" +
-        latVar +
-        "&lon=" +
-        lonVar +
-        "&appid=a0d7c3074ce18b5e27c1432a4af93068";
+        "http://api.openweathermap.org/data/2.5/forecast?q=" +
+        city +
+        "&units=imperial&cnt=5&appid=" +
+        apiKey;
       console.log(queryUrlForecast);
+      $.ajax({
+        url: queryUrlForecast,
+        method: "GET",
+      }).then(function (responseFive) {
+        console.log(responseFive);
+        var date1 = responseFive.list[0].dt_txt;
+        var icon1 = responseFive.list[0].weather[0].icon;
+        var temp1 = responseFive.list[0].main.temp;
+        var hum1 = responseFive.list[0].main.humidity;
+        $("#card1").append(date1, icon1, temp1, hum1);
+      });
     });
   }
 
@@ -86,3 +98,5 @@ $(document).ready(function () {
 });
 
 // api.openweathermap.org/data/2.5/weather?q=Atlanta&units=imperial&appid=a0d7c3074ce18b5e27c1432a4af93068
+
+// On click for history list.  will use a get item
